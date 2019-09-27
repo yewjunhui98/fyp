da86c13ce2ead3e341951f8529295cb73775fa69
DESKTOP-OMVR86G\LORD_DOGGO <yewjunhui98@gmail.com>
2019-09-27 22:40:11 +0800

    Updated the fade height

--- components/Members/Member/Member.js
+++ components/Members/Member/Member.js
@@ -28,7 +28,6 @@ class Member extends Component {
                         {displayposition}
                     </div>
                 </div>
-                
             </div>
         );
     }

61f492be79c80b0e523f80cef047095d881b4cf5
DESKTOP-OMVR86G\LORD_DOGGO <yewjunhui98@gmail.com>
2019-09-30 22:14:27 +0800

    fixed

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,27 +1,6 @@
 import React, {Component} from 'react';
 import classes from './Profile.css';
-import {Route} from 'react-router-dom';
 class profile extends Component {
-state = {
-  name: null
-}
-componentDidMount(){
-  // // const {handle} = this.props.match.params;
-  // const { name } = this.props.location.state;
-
-  // console.log(name);
-  const query = new URLSearchParams(this.props.location.search);
-  console.log("Query: " + query);
-  // for(let param of query.enteries())
-  // {
-  //   if(param[0] === 'name')
-  //   {
-  //     this.setState(prevState => ({
-  //       name: param[1]
-  //     }))
-  //   }
-  // }
-}
     render()
     {
         return(
